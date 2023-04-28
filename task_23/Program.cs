0b115217ee5a8bd6406a4c9e876b3344be9a0f7f
Milamber1990 <andrey.kalva72@gmail.com>
2023-04-28 13:57:39 +0300

    task_23

--- task_23/Program.cs
+++ task_23/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = 1;
+double result = 0;
+while (num2 <= num1)
+{
+    result = Math.Pow(num2,3);
+    Console.Write($" {result}");
+    num2++;
+}
