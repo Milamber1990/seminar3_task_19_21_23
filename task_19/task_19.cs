b9e4522ca31f714b7a094f005d28cd4aeb437a8d
Milamber1990 <andrey.kalva72@gmail.com>
2023-04-28 13:12:20 +0300

    task_19

---
+++ task_19/task_19.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите пятизначное число число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num2 = (num / 1000)%10;
+
+int num4 = (num / 10)%10;
+
+if (num2 == num4)
+Console.WriteLine("YES");
+else
+Console.WriteLine("NO");
